e167b4ec9592f5c466e905789fa082e09628922d
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-04 12:28:58 +0200

    Syntaxe php

--- index.php
+++ index.php
@@ -1,17 +1,64 @@
 <?php
 
-include('./header.php');
+// include './header.php';
+// include_once './header.php';
+
+require './header.php';
+
+// Un commentaire sur une ligne
+/* Un commentaire sur plusieurs lignes */
+
+// echo "<h1>Ceppic PHP</h1>";
 
 
-echo "<h1>Ceppic PHP</h1>";
-?>
+// $variable = 'une variable';
+// $Variable = 'une Variable';
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, harum deleniti facere, optio, reprehenderit laboriosam labore cupiditate ad voluptatem impedit cumque! Porro excepturi, perferendis explicabo autem voluptates natus dolorem facilis.</p>
+// echo $variable . $Variable;
+// echo $variable . " " . $Variable;
+// echo $varible . ' ' . $Variable;
+// echo "$variable";
+// echo '$variable';
+// echo "$variable . $Variable";
+// echo '$variable . $Variable';
 
-<?php
-echo "Encore Ceppic PHP";
+/* $camelCase = 'une variable en camel case';
+$snake_case = 'une variable en snake case';
+$kebab-case = 'une variable en kebab case';
+$PascalCase = 'une variable en pascal case'; */
+
+// $nombre = 45;
+// $nombreDecimal = 10.5;
+// $booleen = false;
+
+// print_r($nombre);
+// var_dump($nombre);
+// var_dump($nombreDecimal);
+// var_dump($booleen);
+// print_r($variable);
+// var_dump($variable);
+
+/* $fruit1 = 'Pomme';
+$fruit2 = 'Banane';
+
+var_dump($fruit1);
+
+echo "<p>$fruit1<br>$fruit2</p>";
+echo "<p>$fruit1" . "<br>" . "$fruit2</p>";
+echo '<p>' . $fruit1 . '<br>' . $fruit2 . '</p>'; */
+
+// Aujourd'hui il fait beau
+// echo "Aujourd'hui il fait beau";
+// echo 'Aujourd\'hui il fait beau';
+
+/* $resultat = '';
+$resultat .= '<h1>';
+$resultat .= 'Cours PHP';
+$resultat .= '</h1>';
+echo "$resultat"; */
+
+echo $titre;
 
 
-include('./footer.php');
 
-?>
+require './footer.php';
