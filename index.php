5a54483944439b9cdf09d080ee35b9d7494c5a15
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-04 15:57:27 +0200

    Ajout de calcul php

--- index.php
+++ index.php
@@ -4,4 +4,27 @@ require './fonction.php';
 
 $titre = "Operateur php";
 
+$x = 4;
+$y = 2;
+$z = 5;
+
+/*
+Les Operateurs :
+
++
+-
+*
+/
+**
+%
+
+*/
+
+$calcul = $x + $y;
+
+
+dd($calcul);
+
+
+
 require './index.view.php';
