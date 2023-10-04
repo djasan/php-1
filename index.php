aa753aa808e5100c863aac1622bc7278433d9ac4
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-04 16:33:12 +0200

    Crée branche tableaux

--- index.php
+++ index.php
@@ -20,11 +20,42 @@ Les Operateurs :
 
 */
 
-$calcul = $x + $y;
+// $calcul = $x + $y;
+// $calcul = $x - $y;
+// $calcul = $x * $y;
+// $calcul = $x / $y;
+// $calcul = $x ** $y;
+// $calcul = $x % $y;
+// $calcul = ($x + $z) * $y;
 
+//$x += 5; // $x = $x + 5
+//$x -=5; // $x = $x - 5
+//$x *= 5; // $x = $x * 5
+//$x /= 5; // $x = $x / 5
+//$x **= 5; // $x = $x ** 5
+//$x %= 5; // $x = $x % 5
 
-dd($calcul);
+// dd($x);
 
+// $i =0;
+// $i++; // $i = $i + 1
+
+// $i--; // $i = $i - 1
+
+// dd($i);
+
+// $anneeDeNaissance = 1990;
+
+// $age = date("Y") - $anneeDeNaissance;
+// dd($age);
+
+//Calcule le prix TTC 
+$prixHT = 34;
+$tva = 10;
+
+$prixTTC = $prixHT + ($tva * $prixHT) / 100;
+
+dd($prixTTC);
 
 
 require './index.view.php';
