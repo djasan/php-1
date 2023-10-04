3b7936e12beb4d7068061a0a866c822955da289e
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-04 10:27:27 +0200

    Iniatialisation

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include('./header.php');
+
+
+echo "<h1>Ceppic PHP</h1>";
+?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, harum deleniti facere, optio, reprehenderit laboriosam labore cupiditate ad voluptatem impedit cumque! Porro excepturi, perferendis explicabo autem voluptates natus dolorem facilis.</p>
+
+<?php
+echo "Encore Ceppic PHP";
+
+
+include('./footer.php');
+
+?>
