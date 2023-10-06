7da6e94b21a2fbbc4a17fdc5d12baf12538f0b86
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-06 15:23:45 +0200

    Boucle foreach

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require './fonction.php';
+
+$titre = "Boucles php";
+
+// $planetes = ['mars','terre','uranus','venus','jupiter','mercure'];
+// $planetes2 = [
+//     'm' => 'mars',
+//     't' => 'terre',
+//     'v' => 'venus',
+//     'j' => 'jupiter',
+//     'u' => 'uranus',
+// ];
+
+$user = [
+    'nom' => 'Doupet Lucien',
+    'email' => 'ldoupe@yaya.com',
+    'competence' => ['php','react','python']
+];
+
+// Afficher dans une liste ul toutes les données de ce user avec foreach
+
+require './index.view.php';
