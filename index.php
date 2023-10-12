02ca1ef8814e40b18705df30e1f4c2a39eddf05d
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-12 14:20:18 +0200

    Ajout de plusieurs imputs

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php 
-require './fonctions.php'; 
+<?php
+require './fonction.php';
 
 // dbug($_GET);
 dbug($_POST);
@@ -7,10 +7,11 @@ dbug($_POST);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
     <title>Formulaire</title>
 </head>
 
@@ -18,13 +19,28 @@ dbug($_POST);
     <h1>Formulaire</h1>
     <form action="" method="POST">
         <label for="nom">Nom :
-        <input type="text" name="nom">
-        </label>
+            <input type="text" name="nom" placeholder="Votre nom">
+        </label><br>
         <label for="email">Email :
-        <input type="email" name="email">
-        </label>
-        <input type="submit" value="Valider">
+            <input type="email" name="email" placeholder="Votre email">
+        </label><br>
+        <label for="commentaire"> Commentaire :</label>
+        <textarea name="commentaire" id="commentaire" cols="30" rows="10"></textarea>
+        <label for="niveau">
+            Niveau :
+            <input type="radio" name="niveau" value="debutant">Débutant
+            <input type="radio" name="niveau" value="Intermediaire">Intermediaire
+            <input type="radio" name="niveau" value="Expert">Expert
+        </label><br>
+        <label for="competences">
+            Compétences :
+            PHP<input type="checkbox" name="competences[]" value="php">
+            Python<input type="checkbox" name="competences[]" value="python">
+            Css<input type="checkbox" name="competences[]" value="css">
+            Javascript<input type="checkbox" name="competences[]" value="Javascript">
+        </label><br>
+        <input type="submit" name="submitted" value="Valider">
     </form>
 </body>
-
 </html>
+
