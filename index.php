a65ef9f3add954ecd63c19bbc5c35b1754c4f0ab
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-24 12:10:26 +0200

    Ajout du routeur

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
 <?php
 require './fonction.php';
 
-// dd($SERVER);
-// dd($_SERVER['REQUEST_URI']);
-// dd(parse_url($_SERVER['REQUEST_URI'])['path']);
+$uriPath = (parse_url($_SERVER['REQUEST_URI'])['path']);
 
-$uri = (parse_url($_SERVER['REQUEST_URI'])['path']);
 
 $routes = [
-'/php' => 'controllers/index.php',
-'/php/conctact' => 'controllers/conctact.php',
-'/php/notes' => 'controllers/notes.php',
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+    '/notes' => 'controllers/notes.php',
 ];
 
+/* $urlToController = match ($uriPath) {
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+    default => 'views/404.php',
+};
+
+require $urlToController;
+exit(); */
+
+function urlToController($uriPath, $routes)
+{
+    if (array_key_exists($uriPath, $routes)) :
+        require $routes[$uriPath];
+    else :
+        abort();
+    endif;
+}
+
+function abort()
+{
+    require 'views/404.php';
+    exit();
+}
+
+urlToController($uriPath, $routes);
