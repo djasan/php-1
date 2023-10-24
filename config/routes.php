2c0f4c8e828f8d89efa9de7b471c2ed236833b30
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-24 12:16:38 +0200

    Ajout d'un dossier config

---
+++ config/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+    '/notes' => 'controllers/notes.php',
+];
---
+++ config/routeur.php
@@ -0,0 +1,29 @@
+<?php
+
+$uriPath = (parse_url($_SERVER['REQUEST_URI'])['path']);
+
+/* $urlToController = match ($uriPath) {
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+    default => 'views/404.php',
+};
+
+require $urlToController;
+exit(); */
+
+function urlToController($uriPath, $routes)
+{
+    if (array_key_exists($uriPath, $routes)) :
+        require $routes[$uriPath];
+    else :
+        abort();
+    endif;
+}
+
+function abort()
+{
+    require 'views/404.php';
+    exit();
+}
+
+urlToController($uriPath, $routes);
