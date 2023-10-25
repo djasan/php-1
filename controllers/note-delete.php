0e1e24f3a592dae462431aa6f9439eafdfde4926
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-25 12:32:05 +0200

    Ajout de la fonction de suppresion de note

--- controllers/note-delete.php
+++ controllers/note-delete.php
@@ -1,8 +1,17 @@
-<?php
+<?php require 'models/Database.php';
 
-require 'models/Database.php';
+$id = $_GET['id'];
 
+if (isset($id)) {
+    $deleteNote = $connexion->prepare('DELETE FROM note WHERE id = :id');
+    $deleteNote->bindParam(':id', $id);
 
-
+    if ($deleteNote->execute()) {
+        header('Location: /notes');
+        exit;
+    } else {
+        echo "Error.";
+    }
+}
 
 require 'views/note-delete.view.php';
--- views/note-delete.view.php
+++ views/note-delete.view.php
@@ -1,4 +1,5 @@
 <?php require 'partials/header.php'; ?>
 
+<h1>La note à était supprimer ! </h1>
 
 <?php require 'partials/footer.php'; ?>
