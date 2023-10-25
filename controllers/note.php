756cbb0cbf33b5e1377a7b5ca24c0bca976236ff
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-25 09:38:36 +0200

    Ajout de l'auteur dans la note

--- controllers/note.php
+++ controllers/note.php
@@ -4,7 +4,7 @@ require 'models/Database.php';
 
 $id = $_GET['id'];
 
-$note = $connexion->prepare('SELECT * FROM note WHERE id = :id');
+$note = $connexion->prepare('SELECT note.*, user.name AS author_name FROM note LEFT JOIN user ON note.user_id = user.user_id WHERE note.id = :id');
 $note->bindParam(':id', $id);
 $note->execute();
 $note = $note->fetch(PDO::FETCH_ASSOC);
--- views/note.view.php
+++ views/note.view.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes</title>
 </head>
+
 <body>
-    <h2><?=$note['title']?></h2>
-    <p><?=$note['content']?></p>
-    <p><?=$note['created_at']?></p>
+    <h2><?= $note['title'] ?></h2>
+    <p><?= $note['content'] ?></p>
+    <p>Publiée le : <?= $note['created_at'] ?> par : <?= $note['author_name'] ?> </p>
     <p><a href="/notes">Retour à la liste des notes</a></p>
 </body>
+
 </html>
