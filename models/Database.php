85c3c175c1daf6d831765752c9ef7d72f03c5972
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-30 14:44:06 +0100

    Ajout d'un option de connexion à la DB

--- models/Database.php
+++ models/Database.php
@@ -1,6 +1,11 @@
 <?php
+require 'config/dbConfig.php';
 
-$user = 'root';
-$pass = '';
-$connexion = new PDO('mysql:host=localhost;dbname=notes', $user, $pass, [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
+$dsn = 'mysql:host=' . DBHOST . ';dbname=' . DBNAME . ';charset=' . DBCHARSET;
 
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+$connexion = new PDO($dsn, DBUSERNAME, DBUSERPASSWORD, $options);
