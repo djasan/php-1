23b8ebb51649a5ba808dc0c64640efdf1b2187fd
Maxime Lottin <maxime.lottin@outlook.com>
2023-11-13 16:16:36 +0100

    Changement de redirection dans mon login

--- controllers/user/login.php
+++ controllers/user/login.php
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login_submit'])) {
             $is_admin = checkIfAdmin($user['user_id'], $connexion);
             $_SESSION['is_admin'] = $is_admin;
 
-            header("Location: /users");
+            header("Location: /notes");
             exit();
         } else {
             $errors[] = "Mot de passe incorrect";
